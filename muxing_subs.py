8b63e978a13216695e9cc2f7f9193fddc97d4708
mijiniko06 <azsuppo9@gmail.com>
2021-12-04 18:15:00 +0800

    Create muxing_subs.py

---
+++ muxing_subs.py
@@ -0,0 +1,24 @@
+import os
+import json
+import subprocess
+import argparse
+import sys
+import pyfiglet
+from rich import print
+from typing import DefaultDict
+
+title = pyfiglet.figlet_format('Muxing Script (For muxing dubs)', font='slant')
+print(f'[red]{title}[/red]')
+print("by Miji-Desu")
+
+mkvmerge = './mkvmerge'
+
+video = input("\nEnter Video Filename : ")
+audio = input("\nEnter Audio Filename : ")
+subtitle = input("\nEnter Subtitle Filename : ")
+lang = input("\nEnter language of the subtitle (like may,eng,zh etc.) : ")
+output = input("\nSpecify Output Filename (without .mkv) : ")
+
+print("Merging .....")
+subprocess.run([mkvmerge, '--ui-language' ,'en_US', '--output', output +'.mkv', '--language', '0:eng', '--track-name', '0:Pipe-HD', '--default-track', '0:yes', '--no-audio', '--compression', '0:none', video, '--language', '0:eng', '--track-name', '0:Pipe-HD', '--default-track', '0:yes', '--compression' ,'0:none', audio, '--language', '0:' + lang, subtitle])
+print("\nAll Done .....")
